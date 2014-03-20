161c223546567053408a43bb40f72c6f656415b8
Bill Dyer <bill.dyer@nokia.com>
2014-03-19 22:09:57 -0500

    Add untrackked files

---
+++ MediaPlayer/GooglePlayMediaProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaPlayerModel;
+
+namespace MediaPlayer
+{
+    public class GooglePlayMediaProvider : IMediaProvider
+    {
+        public ServiceInfo GetServiceInfo()
+        {
+            var si = new ServiceInfo();
+            si.RequiresServerName = false;
+            si.ServiceName = "Google Play";
+            si.ImageData = new byte[0];
+
+            return si;
+        }
+    }
+}
---
+++ MediaPlayer/Views/ViewNavigator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaPlayerPresentation.Helpers;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+
+namespace MediaPlayer.Views
+{
+    class ViewNavigator
+    {
+        public ViewNavigator(INavigationService navigationService)
+        {
+            navigationService.NavigateTo += new EventHandler(OnNavigate);
+        }
+
+        void OnNavigate(object sender, EventArgs e)
+        {
+            var navArgs = e as NavigateEventArgs;
+           
+            var uri = new Uri("/Views/" + navArgs.view +".xaml",UriKind.Relative);
+
+            (Application.Current.RootVisual as PhoneApplicationFrame).Navigate(uri);
+        }
+    }
+}
